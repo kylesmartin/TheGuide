b2719c6b9d91d311e85c907fbdb554943eb8dc75
Kyle Martin <kylesmartin19@gmail.com>
2021-01-18 20:01:27 -0500

    added assets

---
+++ TheGuide/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // public variables
+    public Transform target;
+    public float smoothTime = 0.3f;
+    public Vector3 setPoint = Vector3.zero;
+
+    // private variables
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start(){}
+
+    // Update is called once per frame
+    void Update(){}
+
+    private void LateUpdate()
+    {
+        Vector3 _targetPosition = target.TransformPoint(setPoint);
+        transform.position = Vector3.SmoothDamp(transform.position, _targetPosition, ref velocity, smoothTime);
+        float _y = transform.localEulerAngles.y;
+        transform.LookAt(target);
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, _y, transform.localEulerAngles.z);
+    }
+}
---
+++ TheGuide/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // public variables
+    public float speed = 12f;
+    public float gravity = -19.62f;
+    public float groundCheckRadius = 0.4f;
+    public float jumpHeight = 3f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    // private variables
+    private Vector3 velocity;
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = Vector3.zero;
+        controller = GetComponent<CharacterController>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // reset downward velocity if grounded
+        bool _isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+        if (_isGrounded && velocity.y < 0) velocity.y = -2f;
+        // apply movement inputs
+        float _x = Input.GetAxis("Horizontal");
+        float _z = Input.GetAxis("Vertical");
+        Vector3 _move = _x * transform.right + _z * transform.forward;
+        controller.Move(_move * speed * Time.deltaTime);
+        // apply gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
